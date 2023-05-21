b160092bf486682101d0b49c720958212848c67e
syangl <liusiyangliu@126.com>
2023-05-21 21:36:30 +0800

    UserLogin

---
+++ cet/src/main/java/com/cabse/cet/dao/UserDao.java
@@ -0,0 +1,21 @@
+package com.cabse.cet.dao;
+
+import com.cabse.cet.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @ClassName: UserDao
+ * @PackageName:com.cabse.cet.dao
+ * @Description: TODO
+ * @Author LiuSiyang
+ * @Date 2023/5/21 19:52
+ * @Version 1.0.0
+ */
+
+@Repository
+public interface UserDao extends JpaRepository<User, Integer>{
+    User findByUserName(String userName);
+    User findByUserNameAndPassWord(String userName, String passWord);
+
+}
---
+++ cet/src/main/java/com/cabse/cet/entity/User.java
@@ -0,0 +1,70 @@
+package com.cabse.cet.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * @ClassName: User
+ * @PackageName:com.cabse.cet.entity
+ * @Description: TODO
+ * @Author LiuSiyang
+ * @Date 2023/5/21 19:42
+ * @Version 1.0.0
+ */
+
+@Table(name = "user")
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer uid;
+
+    private String userName;
+
+    private String passWord;
+
+    private Integer state;
+
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid=" + uid +
+                ", userName='" + userName + '\'' +
+                ", passWord='" + passWord + '\'' +
+                ", state=" + state +
+                '}';
+    }
+}
